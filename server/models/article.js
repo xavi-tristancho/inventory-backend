e5c4db1282eab182290150d4382e9ba37503df24
Xavier Tristancho <xavi@letitguide.com>
2016-03-21 22:48:10 +0100

    added automatic timestamps fields

--- server/models/article.js
+++ server/models/article.js
@@ -1,3 +1,14 @@
 module.exports = function(Article) {
 
+	Article.observe('before save', function updateTimestamp(ctx, next) {
+
+		if (ctx.isNewInstance) {
+			ctx.instance.created_at = new Date();
+			ctx.instance.updated_at = new Date();
+		} else {
+			ctx.data.updated_at = new Date();
+		}
+
+		next();
+	});
 };
